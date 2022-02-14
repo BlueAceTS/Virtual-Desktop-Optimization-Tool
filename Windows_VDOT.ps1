9c58f049624786f82bc2b28b4ebd11dcb05649db
BlueAceTS <torsten@blueace.de>
2022-02-14 08:22:26 +0100

    Update Windows_VDOT.ps1

--- Windows_VDOT.ps1
+++ Windows_VDOT.ps1
@@ -33,7 +33,7 @@ Param (
 
     [ValidateSet('All','WindowsMediaPlayer','AppxPackages','ScheduledTasks','DefaultUserSettings','Autologgers','Services','NetworkOptimizations','LGPO','DiskCleanup')] 
     [String[]]
-    $Optimizations = @('WindowsMediaPlayer','AppxPackages','ScheduledTasks','DefaultUserSettings','Autologgers'),
+    $Optimizations = @('WindowsMediaPlayer','AppxPackages','ScheduledTasks','Autologgers','Services','NetworkOptimizations','LGPO','DiskCleanup'),
 
 
     [Switch]$Restart,
